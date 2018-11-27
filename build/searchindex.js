0fcf9e9dd56a5246a623ac0381a7bebc7cb74063
kategerasimenko <katgerasimenko@gmail.com>
2018-11-27 21:21:09 +0400

    built docs

---
+++ build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{_build:0,_doi:0,_pypi:0,content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to lingcorpora&#8217;s documentation!"],titleterms:{document:0,indice:0,lingcorpora:0,tabl:0,welcom:0}})
